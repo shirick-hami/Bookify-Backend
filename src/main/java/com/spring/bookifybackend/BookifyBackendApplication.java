2090edae7b339a06463d3a55df1edfab6cf96750
Saptarick Mishra <saptarickmishra@gmail.com>
2021-07-30 13:07:44 +0530

    Initialized Project with Spring Dependencies

---
+++ src/main/java/com/spring/bookifybackend/BookifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spring.bookifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookifyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookifyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/bookifybackend/BookifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.bookifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
