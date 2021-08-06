0c13376748086096037a1f5f941fde433513b752
Saptarick Mishra <saptarickmishra@gmail.com>
2021-08-06 11:38:28 +0530

    Added some configs in application.properties

--- src/main/java/com/spring/bookifybackend/boostrap/BookifyBackendApplication.java
+++ src/main/java/com/spring/bookifybackend/boostrap/BookifyBackendApplication.java
@@ -1,4 +1,4 @@
-package com.spring.bookifybackend;
+package com.spring.bookifybackend.boostrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
