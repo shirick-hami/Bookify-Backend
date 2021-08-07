140b4ca05e5165103070889f13e9b77c8a766fa1
Saptarick Mishra <saptarickmishra@gmail.com>
2021-08-07 23:45:40 +0530

    Added Spring Security and its config file WebSecurityConfig

--- src/main/java/com/spring/bookifybackend/BookifyBackendApplication.java
+++ src/main/java/com/spring/bookifybackend/BookifyBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.actuate.autoconfigure.security.servlet.Managemen
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
+@SpringBootApplication
 public class BookifyBackendApplication {
 
     public static void main(String[] args) {
